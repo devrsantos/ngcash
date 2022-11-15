37af0234a75d9826c294105c7ea5f7c3d602c828
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-11-14 21:58:58 -0300

    Estrutura Inicial

---
+++ src/config/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+import { routers } from '@routes/routes'
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use((request: Request, response: Response, next) => {
+  response.append('Access-Control-Allow-Origin', ['*'])
+  response.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  response.append('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
+app.use(routers)
+
+export { app }
